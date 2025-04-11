ecdd5146fad268526745c8717df4194a170faa39
Jasshan Kumeresh <jasshank@gmail.com>
2025-04-11 11:56:07 +0800

    nix flake build working for linux

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "unitree sdk dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ccls 
+
+          gcc
+          cmake 
+          gnumake
+          gdb
+            
+          eigen
+        ];
+
+        LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
+        shellHook = ''
+         alias build_sdk="cd build && cmake .. -DCMAKE_INSTALL_PREFIX=./bin && make -j8 && cd .."
+         alias install_sdk="cd build && cmake .. -DCMAKE_INSTALL_PREFIX=./bin && sudo make install && cd .."
+        '';
+      };
+    };
+  }
